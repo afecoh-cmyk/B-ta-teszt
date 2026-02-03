90888bec9b03aa96e6ae84697a3f50a873aca7c4
User <user@example.com>
2026-02-03 21:10:15 +0100

    Remove all generated files from remote

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    // Alapvető konfiguráció, később itt állítjuk be a PWA plugint
-    base: './', // Relatív útvonalak, hogy működjön almappában is
-    server: {
-        host: true
-    }
-});
